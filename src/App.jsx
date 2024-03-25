40ae41b9756adffcd98f16f6ada5a12e0d0b265f
Ahad Quarassi <akberqureshi361@gmail.com>
2024-03-25 14:39:39 +0530

    navbar with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+
+import React from 'react';
+import Navigation from './Components/Navigation';
+
+
+
+function App() {
+
+
+  return (
+    <>
+     <Navigation/>
+    </>
+  )
+}
+
+export default App
